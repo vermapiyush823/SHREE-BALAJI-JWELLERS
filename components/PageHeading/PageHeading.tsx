f4b82660328d854d7535209a2da8eb6f662547dc
vermapiyush823 <vermapiyush823@gmail.com>
2024-08-28 13:03:21 +0530

    commmit

--- components/PageHeading/PageHeading.tsx
+++ components/PageHeading/PageHeading.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import { useParams } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Gemstone from "../../assets/images/Gemstone.jpg";
 import Diamond from "../../assets/images/diamond-jwellery.jpg";
 import GoldBanner from "../../assets/images/gold-jewellery.jpg";
@@ -10,13 +10,15 @@ const PageHeading = () => {
   const { product } = useParams();
   const jwellery_type = product;
   const [jwellery_image, setJwelleryImage] = useState(GoldBanner);
-  if (jwellery_type === "diamond") {
-    setJwelleryImage(Diamond);
-  } else if (jwellery_type === "gemstone") {
-    setJwelleryImage(Gemstone);
-  } else if (jwellery_type === "platinum") {
-    setJwelleryImage(PlatinumBanner);
-  }
+  useEffect(() => {
+    if (jwellery_type === "diamond") {
+      setJwelleryImage(Diamond);
+    } else if (jwellery_type === "gemstone") {
+      setJwelleryImage(Gemstone);
+    } else if (jwellery_type === "platinum") {
+      setJwelleryImage(PlatinumBanner);
+    }
+  }, [jwellery_type]);
   return (
     <div className="w-full h-fit flex justify-center mt-[-4px] relative">
       <Image
